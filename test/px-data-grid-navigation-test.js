30e55f5e51621bac337afd7cb2b60eeee274e823
Drew Harvey <drew@vaadin.com>
2017-12-01 15:47:10 -0800

    adding navigation tests for page size and available page sizes

--- test/px-data-grid-navigation-test.js
+++ test/px-data-grid-navigation-test.js
@@ -18,10 +18,34 @@ function runTests() {
       });
     });
 
-    it('should display proper values in navigation ui', (done) => {
-      expect(navigation.currentPage).to.be.eql(1);
+    function getSelectedPageSize() {
+      return Number.parseInt(
+        navigation.shadowRoot.querySelector('.page-size-select px-dropdown')
+          .shadowRoot.querySelector('#label').innerText);
+    }
+
+    function getAvailablePageSizes() {
+      // wrap return in array since querySelectorAll returns node list not an arrays
+      const options = [...navigation.shadowRoot.querySelector('px-dropdown')
+        .shadowRoot.querySelectorAll('.dropdown-option__item')];
+      return options.map((el) => Number.parseInt(el.innerText));
+    }
+
+    it('should display proper page size', (done) => {
+      // check init page size
+      expect(navigation.pageSize).to.be.eql(getSelectedPageSize());
+      // update page size and check
+      navigation.pageSize = 20;
+      expect(navigation.pageSize).to.be.eql(getSelectedPageSize());
       done();
     });
+
+    it('should display proper user selectable page sizes', (done) => {
+      // check init page size
+      expect(navigation.selectablePageSizes).to.be.eql(getAvailablePageSizes());
+      done();
+    });
+
   });
 
 }
